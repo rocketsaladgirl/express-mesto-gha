5ab08efa9231f9717658f188d5ea865f9d587764
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2023-05-01 21:27:52 +0300

    delete comment in controllers/users.js and modify package.json

--- controllers/users.js
+++ controllers/users.js
@@ -124,13 +124,3 @@ module.exports.updateAvatar = (req, res) => {
         .send({ message: err.message });
     });
 };
-//     .catch((err) => {
-//       if (err.name === 'CastError' || err.name === 'ValidationError') {
-//         res.status(400)
-//           .send({ message: 'Переданы некорректные данные при обновлении аватара' });
-//       } else {
-//         res.status(500)
-//           .send({ message: err.message });
-//       }
-//     });
-// };
