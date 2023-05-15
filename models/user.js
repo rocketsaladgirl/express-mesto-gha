73758671fc9d0d95798234614cf2641bc150f063
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2023-05-15 14:56:13 +0300

    added email and password in models/user.js

--- models/user.js
+++ models/user.js
@@ -8,12 +8,14 @@ const userSchema = new mongoose.Schema({
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
     required: [true, 'Поле "name" должно быть заполнено'],
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
     required: [true, 'Поле "about" должно быть заполнено'],
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
@@ -22,6 +24,21 @@ const userSchema = new mongoose.Schema({
       message: 'Некорректный URL',
     },
     required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
+    type: String,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный email',
+    },
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
   },
 },
 // eslint-disable-next-line function-paren-newline
