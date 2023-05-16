2ded2170531dfc536a537cc5139a31d79ba3d85e
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2023-05-16 21:29:51 +0300

    fixed mistake in models/user

--- models/user.js
+++ models/user.js
@@ -48,7 +48,7 @@ const userSchema = new mongoose.Schema({
 { versionKey: false });
 
 // eslint-disable-next-line func-names
-userSchema.statics.findUserByCredentials = function (email, password) {
+userSchema.statics.findUserByCredentials = function _(email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
