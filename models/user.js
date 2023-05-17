76100cb24f8baa272dd2c52297ae45459dac0a19
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2023-05-17 16:35:19 +0300

    fixed mistakes in models/user, delete required in name,about and avatar

--- models/user.js
+++ models/user.js
@@ -10,14 +10,12 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
-    required: [true, 'Поле "name" должно быть заполнено'],
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
-    required: [true, 'Поле "about" должно быть заполнено'],
     default: 'Исследователь',
   },
   avatar: {
@@ -26,7 +24,6 @@ const userSchema = new mongoose.Schema({
       validator: (v) => validator.isURL(v),
       message: 'Некорректный URL',
     },
-    required: true,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
