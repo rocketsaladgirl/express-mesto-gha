2769d2ff19c3dea46d8a9a61bb4c74f4eee320b4
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2023-05-17 16:13:08 +0300

    fixed mistake in getUserById in controllers/users

--- controllers/users.js
+++ controllers/users.js
@@ -28,9 +28,9 @@ module.exports.getUserById = (req, res, next) => {
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        next(new BadRequestError('Переданы некорректные данные при создании пользователя'));
+        return next(new BadRequestError('Переданы некорректные данные при создании пользователя'));
       }
-      next(err);
+      return next(err);
     });
 };
 
